f167c7d4b7dc124d9a1d0626eeb30873b72fdeb1
sap2411 <steven.parsons.us@gmail.com>
2020-10-03 16:47:32 -0700

    starts LogInPage and component listt

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LogInPage from './components/LogInPage.js'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LogInPage/>
       <StatusBar style="auto" />
     </View>
   );
