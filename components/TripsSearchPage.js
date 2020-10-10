a403e15c147f2c32f56c1af641e25df245bdf371
Colton <kaiserc2@seattleu.edu>
2020-10-10 13:40:16 -0700

    Create boilerplate for TripSearchPage.

---
+++ components/TripsSearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text, StyleSheet} from "react-native"
+
+function TripsSearchPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TripsSearchPage
