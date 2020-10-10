e6f68dd081d693843c9e578b46730fb164214c2b
Colton <kaiserc2@seattleu.edu>
2020-10-10 16:50:04 -0700

    Create TripsSearchPage, text, and styles.

--- components/TripsSearchPage.js
+++ components/TripsSearchPage.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import {View, Text, StyleSheet, Button} from "react-native"
+import { Text, View, StyleSheet, Button } from "react-native";
 
 function TripsSearchPage() {
     return (
-        <Text>Hi</Text>
+        <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>Trips</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    titleContainer: {
+      flex: 1,
+      display: "flex",
+      justifyContent: "space-between",
+      margin: 10,
+      marginLeft: 10,
+    },
+    titleText: {
+      fontSize: 30,
+      alignSelf: "auto",
+      borderWidth: 2, //borderBottomWidth not working on text component
+      borderColor: "gray"
+      // fontFamily: "Pacifico",
+    },
+  });
 
 export default TripsSearchPage
