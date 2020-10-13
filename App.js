a9ef82f7eeec18421e37044dc2a7a4ad01b2fd34
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-10-13 00:14:49 -0700

    adds drawer navigation with icons

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -7,17 +7,46 @@ import LogInPage from "./components/LogInPage.js";
 import TestButton from "./components/TestButton.js";
 import UserAccountPage from "./components/UserAccountPage.js";
 import TripsSearchPage from "./components/TripsSearchPage.js";
+import TripCreationPage from "./components/TripCreationPage.js";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 const { Navigator, Screen } = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Navigator initialRouteName="Home">
-        <Screen name="Home" component={LogInPage} />
-        <Screen name="Account Page" component={UserAccountPage} />
-        <Screen name="Trips" component={TripsSearchPage} />
-      </Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen
+          name="Home"
+          component={LogInPage}
+          options={{
+            drawerIcon: ({ focused, size }) => <Ionicons name="md-snow" />,
+          }}
+        />
+        <Drawer.Screen
+          name="Account Page"
+          component={UserAccountPage}
+          options={{
+            drawerIcon: ({ focused, size }) => <Ionicons name="md-home" />,
+          }}
+        />
+        <Drawer.Screen
+          name="Trips"
+          component={TripsSearchPage}
+          options={{
+            drawerIcon: ({ focused, size }) => <Ionicons name="md-car" />,
+          }}
+        />
+        <Drawer.Screen
+          name="Create Trip"
+          component={TripCreationPage}
+          options={{
+            drawerIcon: ({ focused, size }) => <Ionicons name="md-create" />,
+          }}
+        />
+      </Drawer.Navigator>
       <StatusBar style="auto" />
       <TestButton name="Go to Form" />
     </NavigationContainer>
