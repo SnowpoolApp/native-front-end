6aaa22018a95f5e71eeab52ac9cfe4795f28f072
Colton <kaiserc2@seattleu.edu>
2020-10-14 16:53:53 -0700

    Change data id to string.

--- trips-data.js
+++ trips-data.js
@@ -1,12 +1,12 @@
 const DATA = [
   {
-    id: 1,
+    id: '1',
     startLocation: "Address 1",
     endLocation: "Mountain 1",
     dateAndTime: "10/20/2020, 3:00pm",
   },
   {
-    id: 2,
+    id: '2',
     startLocation: "Address 1",
     endLocation: "Mountain 1",
     dateAndTime: "10/20/2020, 3:00pm",
