e84100460336504b4fa7a61a2362133a992e6f28
Colton <kaiserc2@seattleu.edu>
2020-10-14 16:44:06 -0700

    Change name of data variable to DATA and add ids.

--- trips-data.js
+++ trips-data.js
@@ -1,14 +1,16 @@
-const data = [
+const DATA = [
   {
+    id: 1,
     startLocation: "Address 1",
     endLocation: "Mountain 1",
     dateAndTime: "10/20/2020, 3:00pm",
   },
   {
+    id: 2,
     startLocation: "Address 1",
     endLocation: "Mountain 1",
     dateAndTime: "10/20/2020, 3:00pm",
   },
 ];
 
-export default data
+export default DATA;
