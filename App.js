a6a3a9f04e0e7dfd8f0b30e284c7efaf2ff8b9c9
sap2411 <steven.parsons.us@gmail.com>
2020-10-17 12:07:23 -0700

    conflicts resolved

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import TestButton from "./components/TestButton.js";
 import { RootStackScreen } from './navigation/RootStack.js'
 
 export default () => {
