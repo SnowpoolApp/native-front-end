671a3e4cc8ad1fe326387e9b0b28957349a15079
aidanmc95 <aidanmc95@gmail.com>
2020-10-17 11:54:05 -0700

    Update trips data

--- trips-data.js
+++ trips-data.js
@@ -1,15 +1,25 @@
 const DATA = [
   {
     id: '1',
+    name: "1st Trip",
     startLocation: "Address 1",
     endLocation: "Mountain 1",
     dateAndTime: "10/20/2020, 3:00pm",
+    description: "Description 1",
+    pets: false,
+    roundTrip: true,
+    cost: 18,
   },
   {
     id: '2',
+    name: "2nd Trip",
     startLocation: "Address 2",
     endLocation: "Mountain 2",
     dateAndTime: "10/22/2020, 5:00pm",
+    description: "Description 1",
+    pets: true,
+    roundTrip: true
+    cost: 100
   },
 ];
 
