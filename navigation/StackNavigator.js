a8102cd50ab062d28cff231460ee5d533406aa7c
sap2411 <steven.parsons.us@gmail.com>
2020-10-20 17:21:58 -0700

    navigation options

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import LogInPage from "../components/LogInPage.js";
+import HomePage from "../components/HomePage.js";
 import UserAccountPage from "../components/UserAccountPage.js";
 import TripsSearchPage from "../components/TripsSearchPage.js";
 import TripCreationPage from "../components/TripCreationPage.js";
+import { RootStackScreen } from './RootStack.js'
 
 const Stack = createStackNavigator();
 
 const MainStackNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={LogInPage} />
+      <Stack.Screen name="Home" component={HomePage} />
       <Stack.Screen name="Account Page" component={UserAccountPage} />
       <Stack.Screen name="Trips" component={TripsSearchPage} />
       <Stack.Screen name="Create Trip" component={TripCreationPage} />
